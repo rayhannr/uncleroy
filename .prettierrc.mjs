58a1f2e62d741dbc99c1098ebe5f7b81187c2758
rayhannr <rayhannr07@gmail.com>
2024-03-31 10:32:57 +0700

    fix: move active link style logic to script tag

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,12 +1,21 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
+  singleQuote: true,
+  arrowParens: "always",
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  semi: false,
+  trailingComma: "none",
+  printWidth: 128,
+  proseWrap: "never",
+  plugins: ["prettier-plugin-astro"],
   overrides: [
     {
-      files: '*.astro',
+      files: "*.astro",
       options: {
-        parser: 'astro',
+        parser: "astro",
       },
     },
   ],
-}
+};
