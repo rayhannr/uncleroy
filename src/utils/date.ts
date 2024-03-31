258e87cc6f457971bc1933e3a11c4c2c643061ff
rayhannr <rayhannr07@gmail.com>
2024-03-31 16:41:03 +0700

    fix: fix date timezone

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,7 @@
 export const formatLongDate = (date: Date) =>
   new Intl.DateTimeFormat('en-GB', {
     dateStyle: 'full',
-    timeStyle: 'short'
+    timeStyle: 'long'
   }).format(date)
 
 export const formatShortDate = (date: Date) =>
