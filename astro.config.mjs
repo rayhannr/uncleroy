2bafdaaa1be1cf254394f82d0ede790a1ffcec30
rayhannr <rayhannr07@gmail.com>
2024-04-13 13:32:30 +0700

    feat: finish about page draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,27 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
-import htmlClassNames from "./plugins/html-classnames/index.mjs";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/static'
+import htmlClassNames from './plugins/html-classnames/index.mjs'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      applyBaseStyles: false,
-    }),
+      applyBaseStyles: false
+    })
   ],
   server: {
-    port: 3000,
+    port: 3000
   },
   markdown: {
-    syntaxHighlight: "prism",
-    remarkPlugins: [htmlClassNames],
+    syntaxHighlight: 'prism',
+    remarkPlugins: [htmlClassNames]
   },
-  site:
-    process.env.VERCEL_ENV === "production"
-      ? `https://${process.env.VERCEL_URL}`
-      : process.env.URL,
-  output: "static",
+  site: process.env.VERCEL_ENV === 'production' ? `https://${process.env.VERCEL_URL}` : process.env.URL,
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
-  }),
-});
+      enabled: true
+    }
+  })
+})
