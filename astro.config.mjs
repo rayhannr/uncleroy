38caaf5e34016b94ecc238cc7b311a9da67ae549
rayhannr <rayhannr07@gmail.com>
2024-09-08 14:33:34 +0700

    feat: add scroll progress component inside blog, review, and project pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/static'
 import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
 import expressiveCode from 'astro-expressive-code'
 import htmlClassNames from './plugins/html-classnames.mjs'
 import { externalLink } from './plugins/external-link'
@@ -22,7 +23,8 @@ export default defineConfig({
     sitemap(),
     expressiveCode({
       themes: ['catppuccin-mocha', 'catppuccin-latte']
-    })
+    }),
+    react()
   ],
   server: {
     port: 3000
