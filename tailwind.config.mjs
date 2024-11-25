66d213897547c29f83992cf2fa7cb102fb921596
rayhannr <rayhannr07@gmail.com>
2024-11-25 22:29:50 +0700

    refactor: update mono font family

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './plugins/**/*.mjs'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        mono: ['Geist Mono', ...fontFamily.mono]
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography')],
   darkMode: 'class'
